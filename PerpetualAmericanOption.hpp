327376a49158ce413d9a9ba5ab58e8514b04c15d
Sudhansh Dua <100076760+sudhanshdua@users.noreply.github.com>
2022-03-07 00:57:59 +0530

    Update PerpetualAmericanOption.hpp

--- PerpetualAmericanOption.hpp
+++ PerpetualAmericanOption.hpp
@@ -1,19 +1,24 @@
-//	Class that represents solutions to Perpetual American options
+// Class that defines the derived class: Perpetual American Options
+//
+// (c) Sudhansh Dua
+
 
 #ifndef PerpetualAmericanOption_HPP
 #define PerpetualAmericanOption_HPP
+
 
 #include "Option.hpp"
 #include <string>
 using namespace std;
 
+
 class PerpetualAmericanOption : public Option
 {
 private:
 	// 'Kernel' functions for option calculations
-	double CallPrice() const;								//	Price of a call option
-	double PutPrice() const;								//	Price of a put option
-	void init();											// Initialise all default values
+	double CallPrice() const;					//	Price of a call option
+	double PutPrice() const;					//	Price of a put option
+	void init();							// 	Initialise all default values
 	void copy(const PerpetualAmericanOption& option);		//	copies all values
 
 public:
@@ -23,15 +28,15 @@ public:
 	double K;			//	Strike Price
 	double S;			//	current stock price
 	double b;			//	Cost of carry
-	string type;		//	"C" - call option, "P" - put option
+	string type;			//	"C" - call option, "P" - put option
 
 
 	//	Constructors and the destructor
-	PerpetualAmericanOption();		//	default constructor
-	PerpetualAmericanOption(const PerpetualAmericanOption& option);	//	Copy constructor
+	PerpetualAmericanOption();						//	default constructor
+	PerpetualAmericanOption(const PerpetualAmericanOption& option);		//	Copy constructor
 	PerpetualAmericanOption(const double& S1, const double& K1, const double& r1,
 		const double& sig1, const double& b1, const string type1);	//	constructor that accepts values
-	~PerpetualAmericanOption();		//	destructor
+	~PerpetualAmericanOption();						//	destructor
 
 	//	Assignment operator
 	PerpetualAmericanOption& operator = (const PerpetualAmericanOption& option);
